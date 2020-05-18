d7269cdba9f4d10c2664724c9a7ac3c3c7497b15
vincentdeng2005 <63173529+vincentdeng2005@users.noreply.github.com>
2020-05-18 12:57:47 -0400

    Add files via upload

---
+++ FlashlightScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightScript : MonoBehaviour
+{
+    public GameObject Flashlight;
+    public bool FlashlighIsOpen = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            FlashlighIsOpen = !FlashlighIsOpen;
+
+        if (FlashlighIsOpen)
+            Flashlight.SetActive(true);
+        if(!FlashlighIsOpen)
+            Flashlight.SetActive(false);
+    }
+}
